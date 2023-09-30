d0f55693ede614bf778136907269185823324dbf
IlyushaShulenin <ilya.shulenin36@gmail.com>
2023-09-30 19:41:12 +0300

    final version

---
+++ src/main/java/ru/shulenin/Main.java
@@ -0,0 +1,9 @@
+package ru.shulenin;
+
+import ru.shulenin.lists_analyze.ListsAnalyze;
+
+public class Main {
+    public static void main(String[] args) {
+        TaskRunner.run();
+    }
+}
---
+++ src/test/java/ru/shulenin/lists_analyze/ListsAnalyzeTest.java
@@ -0,0 +1,15 @@
+package ru.shulenin.lists_analyze;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListsAnalyzeTest {
+
+    @org.junit.jupiter.api.Test
+    void addTest() {
+       /*
+        * Если честно, даже не знаю что тут тестировать
+        */
+    }
+}
